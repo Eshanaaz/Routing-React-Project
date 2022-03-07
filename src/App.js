aa3eb0e7c8e096e55ed34445ab3bc6ddca6d198c
Esha Naz <eshanaz76@gmail.com>
2022-03-07 21:17:15 +0500

    work completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import  React from "react";
+import { Routes, Route} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {About, Contact, Services} from "./components/Pages"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="one">First, solve the problem. Then, write the code!</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1 className='alLinks'>HOME PAGE</h1> } />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
+
+
+      </Routes>
     </div>
   );
 }
-
-export default App;
+export default App
